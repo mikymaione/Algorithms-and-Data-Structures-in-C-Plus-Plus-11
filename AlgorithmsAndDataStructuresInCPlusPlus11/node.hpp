a91a58193088505b1cc52c9eaf125ed1ccfc6e03
Michele Maione <mikymaione@hotmail.it>
2023-07-30 13:13:24 +0200

    Binary Tree print

---
+++ AlgorithmsAndDataStructuresInCPlusPlus11/node.hpp
@@ -0,0 +1,80 @@
+/*
+MIT License
+
+Copyright (c) 2023 Michele Maione
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <functional>
+#include <memory>
+#include "node.h"
+
+// costructor
+template<typename T>
+node<T>::node(T z) :v(z) {}
+
+// editing
+template<typename T>
+inline void node<T>::insert(T z)
+{
+	if (v < z)
+	{
+		if (l)
+			l->insert(z);
+		else
+			l = std::make_unique<node>(z);
+	}
+	else
+	{
+		if (r)
+			r->insert(z);
+		else
+			r = std::make_unique<node>(z);
+	}
+}
+
+// print routines
+template<typename T>
+void node<T>::pre_order(std::function<void(T)> printFn)
+{
+	printFn(v);
+
+	if (l)
+		l->in_order(printFn);
+
+	if (r)
+		r->in_order(printFn);
+}
+
+template<typename T>
+void node<T>::in_order(std::function<void(T)> printFn)
+{
+	if (l)
+		l->in_order(printFn);
+
+	printFn(v);
+
+	if (r)
+		r->in_order(printFn);
+}
+
+template<typename T>
+inline void node<T>::post_order(std::function<void(T)> printFn)
+{
+	if (l)
+		l->in_order(printFn);
+
+	if (r)
+		r->in_order(printFn);
+
+	printFn(v);
+}
+
+#endif
